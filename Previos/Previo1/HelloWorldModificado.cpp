2dbe63c6dc69dd18671b59c73a6cde8ebb0841fd
andresbove <89179029+andresbove@users.noreply.github.com>
2024-03-17 16:11:45 -0600

    Update HelloWorldModificado.cpp

--- Previos/Previo1/HelloWorldModificado.cpp
+++ Previos/Previo1/HelloWorldModificado.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <iostream> // Libreria para manejar la entrada y salida de la computadora
 
-int main() {
+int main() { // Función principal para funcionamiento del codigo
     // Se imprime "Hello World" modificado en la consola sin salto de línea.
-    std::cout << "Hello World nuevo para prueba :)";
-    return 0;
+    std::cout << "Hello World nuevo para prueba :)"; // Comando principal para imprimir en consola
+    return 0; // Fin de la función del main
 }
