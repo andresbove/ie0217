accb0994dbba066b5f0fca1a95e0b678e343e544
Andres <andres.bonillavega@ucr.ac.cr.com>
2024-03-17 15:12:15 -0600

    Actualizando Previos con HelloWorld2

---
+++ Previos/Previo1/HelloWorldModificado.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    // Se imprime "Hello World" modificado en la consola sin salto de línea.
+    std::cout << "Hello World nuevo para prueba :)";
+    return 0;
+}
