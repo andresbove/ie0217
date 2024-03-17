e46ddc271c96d4224188177039cce2955ca1d88f
andresbove <89179029+andresbove@users.noreply.github.com>
2024-03-17 16:19:00 -0600

    Update HelloWorld.cpp

--- Previos/Previo1/HelloWorld.cpp
+++ Previos/Previo1/HelloWorld.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <iostream> // Encabezado para incluir funciones para comunicarse con la computadora
 
-int main() {
+int main() { // Función para empezar el programa principal
     // Se imprime "Hello World" en la consola sin salto de línea.
-    std::cout << "Hello World";
-    return 0;
+    std::cout << "Hello World";  // Función para imprimir el mensaje en consola.
+    return 0;  // Comando para terminar la función principal
 }
