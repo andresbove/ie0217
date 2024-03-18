f54fc9e38604b55097d3048e343186bc991001dc
Bonilla Vega <andres.bonilla.vega@intel.com>
2024-03-18 17:39:19 -0600

    Añadiendo Codigo del Previo 2 desde la compu del trabajo

---
+++ Previos/Previo2/TrabajoPrevio2.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // Se incluye la biblioteca 'iostream' en el header para imprimir mensajes en pantalla
+
+using namespace std;
+
+int main (){
+    // Se crean variables 'locales' con respecto al 'main'
+    int a, b; // Se definen las variables 'a' y 'b' como enteros
+    int c; // Se define la variable 'c' como entero
+
+    // Se definen las variables anteriormente creadas
+    a = 10;
+    b = 10;
+    c = a + b;
+    
+    cout << c;
+
+    return 0;
+}
