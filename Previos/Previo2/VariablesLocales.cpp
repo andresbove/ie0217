b8caa36dd677652ac93be8f7e0d0bd3b8a38c93c
Bonilla Vega <andres.bonilla.vega@intel.com>
2024-03-19 15:14:36 -0600

    metiendo cambios de Previos desde la PC del brete

---
+++ Previos/Previo2/VariablesLocales.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // Se incluye la biblioteca 'iostream' en el header para imprimir mensajes en pantalla
+
+using namespace std;
+
+int main (){
+    // Se crean variables 'locales' con respecto al 'main'
+    int a, b; // Se crean las variables 'a' y 'b' como enteros
+    int c; // Se crean la variable 'c' como entero
+
+    // Se definen las variables anteriormente creadas
+    a = 10;
+    b = 10;
+    c = a + b; // Se suman 'a' y 'b' y se guarda el resultado en 'c'.
+    
+    cout << c; // Se imprime la salida con 'cout', gracias a la libreria 'iostream'
+
+    return 0;
+}
