2a8d03909f86d28b53bf244e6d59d695253823b9
Andres <andres.bonillavega@ucr.ac.cr.com>
2024-03-18 18:24:25 -0600

    Metiendo EntradasDeUsurio, PC personal

--- Previos/Previo2/EntradasDeUsuario.cpp
+++ Previos/Previo2/EntradasDeUsuario.cpp
@@ -1,7 +1,10 @@
 #include <iostream> // Se incluye la biblioteca 'iostream' en el header para imprimir mensajes en pantalla
 using namespace std; // Se utiliza esta linea para simplificar el uso de 'cout'
 
+
+int main() {
 int x; // Se crea la variable 'x' como entero
 cout << "Escriba un numero: "; // Se le envia un mensaje al usuario con el operador de salida '<<', concatenado del 'cout'
-cin >> // Se extrae la entrada del usuario con el operador de entrada '>>', concatenando el 'cin'
-cout << "Su numero es: " << x; // Se concatenan las salidas 'Su numero es:' y 'x' con el operador de salida '<<'.
+cin >> x; // Se extrae la entrada del usuario con el operador de entrada '>>', concatenando el 'cin'
+cout << "Su numero es: " << x; // Se concatenan las salidas 'Su numero es:' y 'x' con el operador de salida '<<'
+}
