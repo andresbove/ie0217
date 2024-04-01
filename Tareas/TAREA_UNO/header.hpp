616625e7a187bb4c067b81a67a0069ae9519ac67
Andres <andres.bonillavega@ucr.ac.cr.com>
2024-04-01 10:50:29 -0600

    Metiendo Tareas desde Rama Personal

---
+++ Tareas/TAREA_UNO/header.hpp
@@ -0,0 +1,11 @@
+#ifndef
+#define AHORCADO_H
+// Se define la estructura de "Ahorcado"
+struct Ahorcado
+{
+char nombre[50]; // Se establece el tamaño del array de la palabra a adivinar.
+char estado;
+int IntentosPermitidos;
+int IntentosRealizados;
+};
+#endif
---
+++ Tareas/TAREA_UNO/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include "header.hpp"
+using namespace std;
+
+
+
+
+int main()
+{
+return 0;
+}
