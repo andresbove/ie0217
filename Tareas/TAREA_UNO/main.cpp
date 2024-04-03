d130862ff66d3535a22857d9a19e6dcd422b42ac
Bonilla Vega <andres.bonilla.vega@intel.com>
2024-04-02 18:24:35 -0600

    Actualizacion del codigo, ahora hay un array de strings y funciona aleatoriamente

--- Tareas/TAREA_UNO/main.cpp
+++ Tareas/TAREA_UNO/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <string>
 #include "verificacion.cpp"
+#include <cstdlib> // Para utilizar la función rand()
+#include <ctime>   // Para inicializar la semilla de generación de números aleatorios
 
 int main() 
 {
-    std::string palabra = "hola";
+   // Array de palabras
+    std::string palabras[] = {"hola", "adios", "bienvenido", "programacion", "computadora"};
+
+    // Inicializar la semilla del generador de números aleatorios
+    std::srand(std::time(0));
+
+    // Seleccionar una palabra aleatoria del array
+    std::string palabra = palabras[std::rand() % (sizeof(palabras) / sizeof(palabras[0]))];
+
     verificacion(palabra);
     return 0;
 }
