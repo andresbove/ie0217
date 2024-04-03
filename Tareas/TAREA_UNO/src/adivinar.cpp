ef3f5950fa277d2ebb50c86969de702b1bcbe8b0
Andres <andres.bonillavega@ucr.ac.cr.com>
2024-04-03 07:00:12 -0600

    Subiendo versión final de la tarea

--- Tareas/TAREA_UNO/src/adivinar.cpp
+++ Tareas/TAREA_UNO/src/adivinar.cpp
@@ -1,3 +1,9 @@
+/**
+* @brief Se establece la funcion 'adivinar'
+* Esta se encargara de ver si la palabra se esta adivinando o no
+* @param  palabra es el diccionario ya definido de strings
+*/
+
 #include "header.hpp"
 
 bool adivinar(const std::string& palabra) {
@@ -10,9 +16,9 @@ bool adivinar(const std::string& palabra) {
     // Bucle for para recorrer cada caracter de la palabra
     for (size_t i = 0; i < palabra.length(); ++i) 
     {
-        if (palabra[i] == caracterABuscar) 
+        if (palabra[i] == caracterABuscar)  // Si la entrada del array es igual al caracter ingresado
         {
-            encontrado = true;
+            encontrado = true;  // Se cambia su estado a 'true'
             break; // Si se encuentra el caracter, salimos del bucle
         }
     }
